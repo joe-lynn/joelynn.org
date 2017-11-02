35bf2f61b721416f73d66333b1dd8562453c7f15
joe-lynn <jml1074@rit.edu>
2017-11-01 21:31:52 -0400

    removed aquarelle, excess files

--- js/main.js
+++ js/main.js
@@ -1,30 +1,3 @@
-var fade = document.querySelector('.fade');
-var image = document.getElementsByTagName('img')[0];
-
-var aquarelle = new Aquarelle(image, 'img/mask.png', {
-    fromAmplitude: 0,
-    toAmplitude: 20,
-    fromFrequency: 8,
-    toFrequency: 9,
-    autoplay: true,
-    loop: true,
-    fromOffset: 0,
-    toOffset: 50,
-    duration: 16000
-
-});
-
-aquarelle.addEventListener('created', function() {
-    var canvas = this.getCanvas();
-    canvas.removeAttribute('style');
-    image.parentNode.insertBefore(canvas, image.nextSibling);
-    image.parentNode.removeChild(image);
-});
-
-aquarelle.addEventListener('changed', function(event) {
-
-});
-
 $( document ).ready(function() {
     $('.pull-down').each(function() {
         var $this = $(this);
