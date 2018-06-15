5a0699e909c846ce97b85d64c0114cbfa756eaeb
joe-lynn <jml1074@rit.edu>
2018-06-15 15:40:38 -0400

    resizing window refreshes page, logo moved up, senior project thumbnail and modal added

--- js/main.js
+++ js/main.js
@@ -1,8 +1,33 @@
+window.onresize = function(){ location.reload(); }
 $( document ).ready(function() {
     $('.pull-down').each(function() {
         var $this = $(this);
         $this.css('margin-top', $this.parent().height() - $this.height())
     });
+    // Get the modal
+    var modal = document.getElementById('posterModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById('poster');
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    };
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    var entireSpan = document.getElementById('posterModal');
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+    entireSpan.onclick = function (ev) {
+        modal.style.display = 'none';
+    }
 });
 
 (function titleScroller(text) {
